d7fbc2f0316308861576cf32b310f1f3431242d9
Petro-Herman <159651446+Petro-Herman@users.noreply.github.com>
2025-07-06 00:49:29 +0300

    upd

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,14 @@
 import About from "../../components/About/About";
 import HeaderHome from "../../components/Header/Header";
 import Hero from "../../components/hero/Hero";
+import css from "./Home.module.css"
 
 export default function Home() {
   return (
-    <>
+    <div className={css.container}>
       <HeaderHome />
       <Hero />
       <About />
-    </>
+    </div>
   );
 }
